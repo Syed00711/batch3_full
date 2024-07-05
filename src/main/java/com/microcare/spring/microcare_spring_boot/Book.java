a26507ab5093a46043304343452816d90a510cd5
syed Hussain <syed.hussain@cerner.com>
2024-07-05 08:38:11 +0530

    new repo

---
+++ src/main/java/com/microcare/spring/microcare_spring_boot/Book.java
@@ -0,0 +1,26 @@
+package com.microcare.spring.microcare_spring_boot;
+
+public class Book {
+	private String bookName;
+	private int year;
+	private String author;
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+}
---
+++ src/test/java/com/microcare/spring/microcare_spring_boot/MicrocareSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcare.spring.microcare_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocareSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
